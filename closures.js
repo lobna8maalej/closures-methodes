f4d811ddc5b44da6d8cd60e1395ee5e71a12f8b2
lobna8lobna <lobna89@gmail.com>
2026-02-24 20:21:44 +0100

    envoyer l'essai

---
+++ closures.js
@@ -0,0 +1,50 @@
+function creerCompte(initiale) {
+  var solde = initiale;
+
+  return {
+
+    retirer: function(montant) {
+      if (solde - montant >= 0) {
+        solde = solde - montant;
+        return "Voici votre argent : $" + montant;
+      }
+      return "Solde insuffisant";
+    },
+
+    verifierSolde: function() {
+      return "Votre solde est : $" + solde;
+    },
+
+    deposer: function(montant) {
+      solde = solde + montant;
+      return "Votre solde est : $" + solde;
+    }
+
+  };
+}
+
+function makeCounter(initiale) {
+    var valeur = initiale;   
+    return {
+        up: function () {
+            valeur++;        
+            return valeur;
+        },
+
+        down: function () {
+            valeur--;       
+            return valeur;
+        }
+    };
+}
+
+function Tv() {
+var chanelNumber=0
+return {
+
+watch:function(){
+    console.log('message'+ chanelNumber)
+
+}
+}
+}
